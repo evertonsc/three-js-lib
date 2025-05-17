31dcc7fa4024765fa30f7a136cf995e495928dec
Everton Santos de Castro <evertonsantosdecastro@gmail.com>
2025-05-17 19:50:11 -0300

    Changing imports from main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js';
 
 // Cena, câmera e renderizador
 const scene = new THREE.Scene();
